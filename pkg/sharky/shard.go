ethersphere/bee

fix: sharky rewrite

pkg/sharky/shard.go
@@ -5,7 +5,6 @@
 package sharky
 
 import (
-	"context"
 	"encoding/binary"
 	"io"
 )
@@ -59,30 +58,9 @@ type sharkyFile interface {
 	Sync() error
 }
 
-// write models the input to a write operation
-type write struct {
-	buf []byte     // variable size read buffer
-	res chan entry // to put the result through
-}
-
-// entry models the output result of a write operation
-type entry struct {
-	loc Location // shard, slot, length combo
-	err error    // signal for end of operation
-}
-
-// read models the input to read operation (the output is an error)
-type read struct {
-	ctx  context.Context
-	buf  []byte // variable size read buffer
-	slot uint32 // slot to read from
-}
-
 // shard models a shard writing to a file with periodic offsets due to fixed maxDataSize
 type shard struct {
-	reads       chan read     // channel for reads
-	errc        chan error    // result for reads
-	writes      chan write    // channel for writes
+	available   chan availableShard
 	index       uint8         // index of the shard
 	maxDataSize int           // max size of blobs
 	file        sharkyFile    // the file handle the shard is writing data to
@@ -92,54 +70,13 @@ type shard struct {
 
 // forever loop processing
 func (sh *shard) process() {
-	var writes chan write
-	var slot uint32
-	defer func() {
-		// this condition checks if an slot is in limbo (popped but not used for write op)
-		if writes != nil {
-			sh.slots.limboWG.Add(1)
-			go func() {
-				defer sh.slots.limboWG.Done()
-				sh.slots.in <- slot
-			}()
-		}
-	}()
-	free := sh.slots.out
+	defer sh.close()
 
 	for {
+		slot := sh.slots.Next()
 		select {
-		case op := <-sh.reads:
-			select {
-			case sh.errc <- sh.read(op):
-			case <-op.ctx.Done():
-				// since the goroutine in the Read method can quit
-				// on shutdown, we need to make sure that we can actually
-				// write to the channel, since a shutdown is possible in
-				// theory between after the point that the context is cancelled
-				select {
-				case sh.errc <- op.ctx.Err():
-				case <-sh.quit:
-					// since the Read method respects the quit channel
-					// we can safely quit here without writing to the channel
-					return
-				}
-			case <-sh.quit:
-				return
-			}
-
-			// only enabled if there is a free slot previously popped
-		case op := <-writes:
-			op.res <- sh.write(op.buf, slot)
-			free = sh.slots.out // reenable popping a free slot next time we can write
-			writes = nil        // disable popping a write operation until there is a free slot
-
-			// pop a free slot
-		case slot = <-free:
-			// only if there is one can we pop a chunk to write otherwise keep back pressure on writes
-			// effectively enforcing another shard to be chosen
-			writes = sh.writes // enable popping a write operation
-			free = nil         // disabling getting a new slot until a write is actually done
-
+		case sh.available <- availableShard{shardIndex: sh.index, slot: slot}:
+			sh.slots.Use(slot)
 		case <-sh.quit:
 			return
 		}
@@ -149,8 +86,7 @@ func (sh *shard) process() {
 // close closes the shard:
 // wait for pending operations to finish then saves free slots and blobs on disk
 func (sh *shard) close() error {
-	sh.slots.wg.Wait()
-	if err := sh.slots.save(); err != nil {
+	if err := sh.slots.Save(); err != nil {
 		return err
 	}
 	if err := sh.slots.file.Close(); err != nil {
@@ -166,30 +102,22 @@ func (sh *shard) offset(slot uint32) int64 {
 }
 
 // read reads loc.Length bytes to the buffer from the blob slot loc.Slot
-func (sh *shard) read(r read) error {
-	_, err := sh.file.ReadAt(r.buf, sh.offset(r.slot))
+func (sh *shard) read(buf []byte, slot uint32) error {
+	_, err := sh.file.ReadAt(buf, sh.offset(slot))
 	return err
 }
 
 // write writes loc.Length bytes to the buffer from the blob slot loc.Slot
-func (sh *shard) write(buf []byte, slot uint32) entry {
+func (sh *shard) write(buf []byte, slot uint32) (Location, error) {
 	n, err := sh.file.WriteAt(buf, sh.offset(slot))
-	return entry{
-		loc: Location{
-			Shard:  sh.index,
-			Slot:   slot,
-			Length: uint16(n),
-		},
-		err: err,
-	}
+	return Location{
+		Shard:  sh.index,
+		Slot:   slot,
+		Length: uint16(n),
+	}, err
 }
 
 // release frees the slot allowing new entry to overwrite
-func (sh *shard) release(ctx context.Context, slot uint32) error {
-	select {
-	case sh.slots.in <- slot:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	}
+func (sh *shard) release(slot uint32) {
+	sh.slots.Free(slot)
 }

pkg/sharky/store.go
@@ -11,8 +11,7 @@ import (
 	"io/fs"
 	"strconv"
 	"sync"
-
-	"github.com/hashicorp/go-multierror"
+	"time"
 )
 
 var (
@@ -28,12 +27,17 @@ var (
 // - read prioritisation over writing
 // - free slots allow write
 type Store struct {
-	maxDataSize int             // max length of blobs
-	writes      chan write      // shared write operations channel
-	shards      []*shard        // shards
-	wg          *sync.WaitGroup // count started operations
-	quit        chan struct{}   // quit channel
-	metrics     metrics
+	maxDataSize     int            // max length of blobs
+	shards          []*shard       // shards
+	wg              sync.WaitGroup // count started operations
+	quit            chan struct{}  // quit channel
+	metrics         metrics
+	availableShards chan availableShard
+}
+
+type availableShard struct {
+	shardIndex uint8
+	slot       uint32
 }
 
 // New constructs a sharded blobstore
@@ -44,12 +48,11 @@ type Store struct {
 // - maxDataSize - positive integer representing the maximum blob size to be stored
 func New(basedir fs.FS, shardCnt int, maxDataSize int) (*Store, error) {
 	store := &Store{
-		maxDataSize: maxDataSize,
-		writes:      make(chan write),
-		shards:      make([]*shard, shardCnt),
-		wg:          &sync.WaitGroup{},
-		quit:        make(chan struct{}),
-		metrics:     newMetrics(),
+		maxDataSize:     maxDataSize,
+		shards:          make([]*shard, shardCnt),
+		quit:            make(chan struct{}),
+		metrics:         newMetrics(),
+		availableShards: make(chan availableShard),
 	}
 	for i := range store.shards {
 		s, err := store.create(uint8(i), maxDataSize, basedir)
@@ -66,12 +69,19 @@ func New(basedir fs.FS, shardCnt int, maxDataSize int) (*Store, error) {
 // Close closes each shard and return incidental errors from each shard
 func (s *Store) Close() error {
 	close(s.quit)
-	err := new(multierror.Error)
-	for _, sh := range s.shards {
-		err = multierror.Append(err, sh.close())
-	}
 
-	return err.ErrorOrNil()
+	done := make(chan struct{})
+	go func() {
+		s.wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case <-time.After(time.Second * 5):
+		return errors.New("did not close on time")
+	}
 }
 
 // create creates a new shard with index, max capacity limit, file within base directory
@@ -84,64 +94,32 @@ func (s *Store) create(index uint8, maxDataSize int, basedir fs.FS) (*shard, err
 	if err != nil {
 		return nil, err
 	}
-	sl := newSlots(ffile.(sharkyFile), s.wg)
+	sl := newSlots(ffile.(sharkyFile))
 	err = sl.load()
 	if err != nil {
 		return nil, err
 	}
 	sh := &shard{
-		reads:       make(chan read),
-		errc:        make(chan error),
-		writes:      s.writes,
+		available:   s.availableShards,
 		index:       index,
 		maxDataSize: maxDataSize,
 		file:        file.(sharkyFile),
 		slots:       sl,
 		quit:        s.quit,
 	}
-	terminated := make(chan struct{})
-	sh.slots.wg.Add(1)
+	s.wg.Add(1)
 	go func() {
-		defer sh.slots.wg.Done()
+		defer s.wg.Done()
 		sh.process()
-		close(terminated)
-	}()
-	sh.slots.wg.Add(1)
-	go func() {
-		defer sh.slots.wg.Done()
-		sl.process(terminated)
 	}()
 	return sh, nil
 }
 
 // Read reads the content of the blob found at location into the byte buffer given
 // The location is assumed to be obtained by an earlier Write call storing the blob
-func (s *Store) Read(ctx context.Context, loc Location, buf []byte) (err error) {
+func (s *Store) Read(loc Location, buf []byte) (err error) {
 	sh := s.shards[loc.Shard]
-	select {
-	case sh.reads <- read{ctx: ctx, buf: buf[:loc.Length], slot: loc.Slot}:
-		s.metrics.TotalReadCalls.Inc()
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-sh.quit:
-		return ErrQuitting
-	}
-
-	// it is important that this select would NEVER respect the context
-	// cancellation. this would result in a deadlock on the shard, since
-	// the result of the operation must be drained from errc, allowing the
-	// shard to be able to handle new operations (#2932).
-	select {
-	case err = <-sh.errc:
-		if err != nil {
-			s.metrics.TotalReadCallsErr.Inc()
-		}
-		return err
-	case <-s.quit:
-		// we need to make sure that the forever loop in shard.go can
-		// always return due to shutdown in case this goroutine goes away.
-		return ErrQuitting
-	}
+	return sh.read(buf[:loc.Length], loc.Slot)
 }
 
 // Write stores a new blob and returns its location to be used as a reference
@@ -150,34 +128,15 @@ func (s *Store) Write(ctx context.Context, data []byte) (loc Location, err error
 	if len(data) > s.maxDataSize {
 		return loc, ErrTooLong
 	}
+
 	s.wg.Add(1)
 	defer s.wg.Done()
 
-	c := make(chan entry, 1) // buffer the channel to avoid blocking in shard.process on quit or context done
-
 	select {
-	case s.writes <- write{data, c}:
-		s.metrics.TotalWriteCalls.Inc()
-	case <-s.quit:
-		return loc, ErrQuitting
+	case available := <-s.availableShards:
+		return s.shards[available.shardIndex].write(data, available.slot)
 	case <-ctx.Done():
-		return loc, ctx.Err()
-	}
-
-	select {
-	case e := <-c:
-		if e.err == nil {
-			shard := strconv.Itoa(int(e.loc.Shard))
-			s.metrics.CurrentShardSize.WithLabelValues(shard).Inc()
-			s.metrics.ShardFragmentation.WithLabelValues(shard).Add(float64(s.maxDataSize - int(e.loc.Length)))
-		} else {
-			s.metrics.TotalWriteCallsErr.Inc()
-		}
-		return e.loc, e.err
-	case <-s.quit:
-		return loc, ErrQuitting
-	case <-ctx.Done():
-		return loc, ctx.Err()
+		return Location{}, ctx.Err()
 	}
 }
 
@@ -187,16 +146,11 @@ func (s *Store) Write(ctx context.Context, data []byte) (loc Location, err error
 // Note that releasing is not safe for obfuscating earlier content, since
 // even after reuse, the slot may be used by a very short blob and leaves the
 // rest of the old blob bytes untouched
-func (s *Store) Release(ctx context.Context, loc Location) error {
+func (s *Store) Release(loc Location) {
 	sh := s.shards[loc.Shard]
-	err := sh.release(ctx, loc.Slot)
+	sh.release(loc.Slot)
 	s.metrics.TotalReleaseCalls.Inc()
-	if err == nil {
-		shard := strconv.Itoa(int(sh.index))
-		s.metrics.CurrentShardSize.WithLabelValues(shard).Dec()
-		s.metrics.ShardFragmentation.WithLabelValues(shard).Sub(float64(s.maxDataSize - int(loc.Length)))
-	} else {
-		s.metrics.TotalReleaseCallsErr.Inc()
-	}
-	return err
+	shard := strconv.Itoa(int(sh.index))
+	s.metrics.CurrentShardSize.WithLabelValues(shard).Dec()
+	s.metrics.ShardFragmentation.WithLabelValues(shard).Sub(float64(s.maxDataSize - int(loc.Length)))
 }

orgTestCodacy11KRepos110MB/repo-9226-bee

pkg/sharky/shard.go
@@ -5,7 +5,6 @@
 package sharky
 
 import (
-	"context"
 	"encoding/binary"
 	"io"
 )
@@ -59,30 +58,9 @@ type sharkyFile interface {
 	Sync() error
 }
 
-// write models the input to a write operation
-type write struct {
-	buf []byte     // variable size read buffer
-	res chan entry // to put the result through
-}
-
-// entry models the output result of a write operation
-type entry struct {
-	loc Location // shard, slot, length combo
-	err error    // signal for end of operation
-}
-
-// read models the input to read operation (the output is an error)
-type read struct {
-	ctx  context.Context
-	buf  []byte // variable size read buffer
-	slot uint32 // slot to read from
-}
-
 // shard models a shard writing to a file with periodic offsets due to fixed maxDataSize
 type shard struct {
-	reads       chan read     // channel for reads
-	errc        chan error    // result for reads
-	writes      chan write    // channel for writes
+	available   chan availableShard
 	index       uint8         // index of the shard
 	maxDataSize int           // max size of blobs
 	file        sharkyFile    // the file handle the shard is writing data to
@@ -92,54 +70,13 @@ type shard struct {
 
 // forever loop processing
 func (sh *shard) process() {
-	var writes chan write
-	var slot uint32
-	defer func() {
-		// this condition checks if an slot is in limbo (popped but not used for write op)
-		if writes != nil {
-			sh.slots.limboWG.Add(1)
-			go func() {
-				defer sh.slots.limboWG.Done()
-				sh.slots.in <- slot
-			}()
-		}
-	}()
-	free := sh.slots.out
+	defer sh.close()
 
 	for {
+		slot := sh.slots.Next()
 		select {
-		case op := <-sh.reads:
-			select {
-			case sh.errc <- sh.read(op):
-			case <-op.ctx.Done():
-				// since the goroutine in the Read method can quit
-				// on shutdown, we need to make sure that we can actually
-				// write to the channel, since a shutdown is possible in
-				// theory between after the point that the context is cancelled
-				select {
-				case sh.errc <- op.ctx.Err():
-				case <-sh.quit:
-					// since the Read method respects the quit channel
-					// we can safely quit here without writing to the channel
-					return
-				}
-			case <-sh.quit:
-				return
-			}
-
-			// only enabled if there is a free slot previously popped
-		case op := <-writes:
-			op.res <- sh.write(op.buf, slot)
-			free = sh.slots.out // reenable popping a free slot next time we can write
-			writes = nil        // disable popping a write operation until there is a free slot
-
-			// pop a free slot
-		case slot = <-free:
-			// only if there is one can we pop a chunk to write otherwise keep back pressure on writes
-			// effectively enforcing another shard to be chosen
-			writes = sh.writes // enable popping a write operation
-			free = nil         // disabling getting a new slot until a write is actually done
-
+		case sh.available <- availableShard{shard: sh.index, slot: slot}:
+			sh.slots.Use(slot)
 		case <-sh.quit:
 			return
 		}
@@ -149,8 +86,7 @@ func (sh *shard) process() {
 // close closes the shard:
 // wait for pending operations to finish then saves free slots and blobs on disk
 func (sh *shard) close() error {
-	sh.slots.wg.Wait()
-	if err := sh.slots.save(); err != nil {
+	if err := sh.slots.Save(); err != nil {
 		return err
 	}
 	if err := sh.slots.file.Close(); err != nil {
@@ -166,30 +102,22 @@ func (sh *shard) offset(slot uint32) int64 {
 }
 
 // read reads loc.Length bytes to the buffer from the blob slot loc.Slot
-func (sh *shard) read(r read) error {
-	_, err := sh.file.ReadAt(r.buf, sh.offset(r.slot))
+func (sh *shard) read(buf []byte, slot uint32) error {
+	_, err := sh.file.ReadAt(buf, sh.offset(slot))
 	return err
 }
 
 // write writes loc.Length bytes to the buffer from the blob slot loc.Slot
-func (sh *shard) write(buf []byte, slot uint32) entry {
+func (sh *shard) write(buf []byte, slot uint32) (Location, error) {
 	n, err := sh.file.WriteAt(buf, sh.offset(slot))
-	return entry{
-		loc: Location{
-			Shard:  sh.index,
-			Slot:   slot,
-			Length: uint16(n),
-		},
-		err: err,
-	}
+	return Location{
+		Shard:  sh.index,
+		Slot:   slot,
+		Length: uint16(n),
+	}, err
 }
 
 // release frees the slot allowing new entry to overwrite
-func (sh *shard) release(ctx context.Context, slot uint32) error {
-	select {
-	case sh.slots.in <- slot:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	}
+func (sh *shard) release(slot uint32) {
+	sh.slots.Free(slot)
 }

pkg/sharky/store.go
@@ -11,8 +11,7 @@ import (
 	"io/fs"
 	"strconv"
 	"sync"
-
-	"github.com/hashicorp/go-multierror"
+	"time"
 )
 
 var (
@@ -28,12 +27,17 @@ var (
 // - read prioritisation over writing
 // - free slots allow write
 type Store struct {
-	maxDataSize int             // max length of blobs
-	writes      chan write      // shared write operations channel
-	shards      []*shard        // shards
-	wg          *sync.WaitGroup // count started operations
-	quit        chan struct{}   // quit channel
-	metrics     metrics
+	maxDataSize     int            // max length of blobs
+	shards          []*shard       // shards
+	wg              sync.WaitGroup // count started operations
+	quit            chan struct{}  // quit channel
+	metrics         metrics
+	availableShards chan availableShard
+}
+
+type availableShard struct {
+	shard uint8
+	slot  uint32
 }
 
 // New constructs a sharded blobstore
@@ -44,12 +48,11 @@ type Store struct {
 // - maxDataSize - positive integer representing the maximum blob size to be stored
 func New(basedir fs.FS, shardCnt int, maxDataSize int) (*Store, error) {
 	store := &Store{
-		maxDataSize: maxDataSize,
-		writes:      make(chan write),
-		shards:      make([]*shard, shardCnt),
-		wg:          &sync.WaitGroup{},
-		quit:        make(chan struct{}),
-		metrics:     newMetrics(),
+		maxDataSize:     maxDataSize,
+		shards:          make([]*shard, shardCnt),
+		quit:            make(chan struct{}),
+		metrics:         newMetrics(),
+		availableShards: make(chan availableShard),
 	}
 	for i := range store.shards {
 		s, err := store.create(uint8(i), maxDataSize, basedir)
@@ -66,12 +69,19 @@ func New(basedir fs.FS, shardCnt int, maxDataSize int) (*Store, error) {
 // Close closes each shard and return incidental errors from each shard
 func (s *Store) Close() error {
 	close(s.quit)
-	err := new(multierror.Error)
-	for _, sh := range s.shards {
-		err = multierror.Append(err, sh.close())
-	}
 
-	return err.ErrorOrNil()
+	done := make(chan struct{})
+	go func() {
+		s.wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case <-time.After(time.Second * 5):
+		return errors.New("did not close on time")
+	}
 }
 
 // create creates a new shard with index, max capacity limit, file within base directory
@@ -84,64 +94,32 @@ func (s *Store) create(index uint8, maxDataSize int, basedir fs.FS) (*shard, err
 	if err != nil {
 		return nil, err
 	}
-	sl := newSlots(ffile.(sharkyFile), s.wg)
+	sl := newSlots(ffile.(sharkyFile))
 	err = sl.load()
 	if err != nil {
 		return nil, err
 	}
 	sh := &shard{
-		reads:       make(chan read),
-		errc:        make(chan error),
-		writes:      s.writes,
+		available:   s.availableShards,
 		index:       index,
 		maxDataSize: maxDataSize,
 		file:        file.(sharkyFile),
 		slots:       sl,
 		quit:        s.quit,
 	}
-	terminated := make(chan struct{})
-	sh.slots.wg.Add(1)
+	s.wg.Add(1)
 	go func() {
-		defer sh.slots.wg.Done()
+		defer s.wg.Done()
 		sh.process()
-		close(terminated)
-	}()
-	sh.slots.wg.Add(1)
-	go func() {
-		defer sh.slots.wg.Done()
-		sl.process(terminated)
 	}()
 	return sh, nil
 }
 
 // Read reads the content of the blob found at location into the byte buffer given
 // The location is assumed to be obtained by an earlier Write call storing the blob
-func (s *Store) Read(ctx context.Context, loc Location, buf []byte) (err error) {
+func (s *Store) Read(loc Location, buf []byte) (err error) {
 	sh := s.shards[loc.Shard]
-	select {
-	case sh.reads <- read{ctx: ctx, buf: buf[:loc.Length], slot: loc.Slot}:
-		s.metrics.TotalReadCalls.Inc()
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-sh.quit:
-		return ErrQuitting
-	}
-
-	// it is important that this select would NEVER respect the context
-	// cancellation. this would result in a deadlock on the shard, since
-	// the result of the operation must be drained from errc, allowing the
-	// shard to be able to handle new operations (#2932).
-	select {
-	case err = <-sh.errc:
-		if err != nil {
-			s.metrics.TotalReadCallsErr.Inc()
-		}
-		return err
-	case <-s.quit:
-		// we need to make sure that the forever loop in shard.go can
-		// always return due to shutdown in case this goroutine goes away.
-		return ErrQuitting
-	}
+	return sh.read(buf[:loc.Length], loc.Slot)
 }
 
 // Write stores a new blob and returns its location to be used as a reference
@@ -150,34 +128,24 @@ func (s *Store) Write(ctx context.Context, data []byte) (loc Location, err error
 	if len(data) > s.maxDataSize {
 		return loc, ErrTooLong
 	}
+
 	s.wg.Add(1)
 	defer s.wg.Done()
 
-	c := make(chan entry, 1) // buffer the channel to avoid blocking in shard.process on quit or context done
-
 	select {
-	case s.writes <- write{data, c}:
-		s.metrics.TotalWriteCalls.Inc()
-	case <-s.quit:
-		return loc, ErrQuitting
-	case <-ctx.Done():
-		return loc, ctx.Err()
-	}
-
-	select {
-	case e := <-c:
-		if e.err == nil {
-			shard := strconv.Itoa(int(e.loc.Shard))
-			s.metrics.CurrentShardSize.WithLabelValues(shard).Inc()
-			s.metrics.ShardFragmentation.WithLabelValues(shard).Add(float64(s.maxDataSize - int(e.loc.Length)))
-			s.metrics.LastAllocatedShardSlot.WithLabelValues(shard).Set(float64(e.loc.Slot))
-		} else {
+	case write := <-s.availableShards:
+		loc, err := s.shards[write.shard].write(data, write.slot)
+		if err != nil {
 			s.metrics.TotalWriteCallsErr.Inc()
+			return loc, err
 		}
-		return e.loc, e.err
-	case <-s.quit:
-		return loc, ErrQuitting
+		shard := strconv.Itoa(int(loc.Shard))
+		s.metrics.CurrentShardSize.WithLabelValues(shard).Inc()
+		s.metrics.ShardFragmentation.WithLabelValues(shard).Add(float64(s.maxDataSize - int(loc.Length)))
+		s.metrics.LastAllocatedShardSlot.WithLabelValues(shard).Set(float64(loc.Slot))
+		return loc, nil
 	case <-ctx.Done():
+		s.metrics.TotalWriteCallsErr.Inc()
 		return loc, ctx.Err()
 	}
 }
@@ -188,17 +156,12 @@ func (s *Store) Write(ctx context.Context, data []byte) (loc Location, err error
 // Note that releasing is not safe for obfuscating earlier content, since
 // even after reuse, the slot may be used by a very short blob and leaves the
 // rest of the old blob bytes untouched
-func (s *Store) Release(ctx context.Context, loc Location) error {
-	sh := s.shards[loc.Shard]
-	err := sh.release(ctx, loc.Slot)
+func (s *Store) Release(loc Location) {
 	s.metrics.TotalReleaseCalls.Inc()
-	if err == nil {
-		shard := strconv.Itoa(int(sh.index))
-		s.metrics.CurrentShardSize.WithLabelValues(shard).Dec()
-		s.metrics.ShardFragmentation.WithLabelValues(shard).Sub(float64(s.maxDataSize - int(loc.Length)))
-		s.metrics.LastReleasedShardSlot.WithLabelValues(shard).Set(float64(loc.Slot))
-	} else {
-		s.metrics.TotalReleaseCallsErr.Inc()
-	}
-	return err
+	sh := s.shards[loc.Shard]
+	sh.release(loc.Slot)
+	shard := strconv.Itoa(int(sh.index))
+	s.metrics.CurrentShardSize.WithLabelValues(shard).Dec()
+	s.metrics.ShardFragmentation.WithLabelValues(shard).Sub(float64(s.maxDataSize - int(loc.Length)))
+	s.metrics.LastReleasedShardSlot.WithLabelValues(shard).Set(float64(loc.Slot))
 }